beda59b5689732a5f7e01c055b81d17fb1f126e3
philipwisner <philipwisner@gmail.com>
2018-12-27 17:16:32 -0500

    adding delete tab

--- client/src/Components/EditableText/EditableText.js
+++ client/src/Components/EditableText/EditableText.js
@@ -21,6 +21,12 @@ class EditableText extends Component {
     }
   }
 
+  onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.submit();
+    }
+  }
+
   updateText = (event) => {
     this.setState({text: event.target.value})
   }
@@ -29,6 +35,28 @@ class EditableText extends Component {
     this.setState(prevState => ({
       editing: !prevState.editing
     }))
+  }
+
+  confrimDelete = () => {
+    console.log('confirm delete button clicked');
+    //show modal to confirm deleting tab
+    this.delete();
+  }
+
+  delete = () => {
+    console.log('deleting tab');
+    let {
+      parentResource,
+      parentId,
+      id,
+    } = this.props;
+    if (parentResource === 'room') {
+      console.log('delete inside room ran, parentId and id are', parentId, id);
+      this.props.updateRoomTab(parentId, id, {isTrashed: true})
+    } else if (parentResource === 'activity') {
+      console.log("updatingActivtiy")
+      this.props.updateActivityTab(parentId, id, {isTrashed: true})
+    }
   }
 
   submit = () => {
@@ -57,11 +85,12 @@ class EditableText extends Component {
             <b>{this.props.title}</b>
             {this.props.inputType === 'TEXT_AREA'
               ? <textarea className={classes.TextArea} onChange={this.updateText} value={this.state.text}/>
-              : <input className={classes.TextInput} onChange={this.updateText} value={this.state.text}/>
+              : <input className={classes.TextInput} onChange={this.updateText} onKeyPress={this.onKeyPress} value={this.state.text}/>
             }
             <div className={classes.EditButtons}>
               <div><Button m={'0 10px'} click={this.submit}>Save</Button></div>
               <div><Button m={0} click={this.toggleEdit} theme='SmallCancel'>Cancel</Button></div>
+              <div><Button m={0} click={this.confrimDelete}>Delete</Button></div>
             </div>
           </div>
           : <div>
